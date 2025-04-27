24b831c1b5a8fb35f52ab8153247b31bf8aabc07
AlexNompe <Alex.Nompe@yandex.ru>
2025-04-27 13:17:34 +0300

    Initial commit

---
+++ src/main/java/norivensuu/iinpulib/Iinpulib.java
@@ -0,0 +1,10 @@
+package norivensuu.iinpulib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Iinpulib implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/norivensuu/iinpulib/client/IinpulibClient.java
@@ -0,0 +1,10 @@
+package norivensuu.iinpulib.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IinpulibClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
